891094d6e9fa8f439b1d081538acc5b1bcc7c277
Anthony Scinocco <scinocco.a@gmail.com>
2016-11-11 15:27:57 -0500

    shared dir working

--- mern/docker/scripts/build.sh
+++ mern/docker/scripts/build.sh
@@ -12,15 +12,18 @@
 # <new> <local>: will build the image from scratch with local settings ------- #
 # <   > <     >: will build the image for local settings --------------------- #
 # ---------------------------------------------------------------------------- #
+APP_PATH="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
 DOCKER_DIR="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
 ARGS_FILE_PATH="./docker/args.dev.list"
-docker stop $(docker ps -a -q)
+# docker stop $(docker ps -a -q)
 # docker rm $(docker ps -a -q)
-DEV_ARGS='--build-arg NODE_ENV_VAR="development" --build-arg CODE_DIR="./code"'
-
+DEV_ARGS='--build-arg NODE_ENV_VAR="development"'
 # docker build --build-arg NODE_ENV_VAR=ENV_VAL --build-arg CODE_DIR=$CODE_DIR -t anthonyscinocco/mern:dev "$DOCKER_DIR"
 
 docker build $(echo $DEV_ARGS) -t anthonyscinocco/mern:dev "$DOCKER_DIR"
+docker run -d -v "$(echo $APP_PATH)":/usr/src/app \
+        -i -t anthonyscinocco/mern:dev /bin/bash
+
 
 
 # -------------------------------- #
@@ -33,6 +36,22 @@ docker build $(echo $DEV_ARGS) -t anthonyscinocco/mern:dev "$DOCKER_DIR"
 #   #statements
 # }
 
+# -------------------------------- #
+# buildProduction () ------------- #
+# builds the container for the --- #
+# prod environment --------------- #
+# Paramter: $new (boolean) ------- #
+# -------------------------------- #
 # function buildProduction {
+#   #statements
+# }
+
+# -------------------------------- #
+# run () ------------------------- #
+# handles control flow ----------- #
+# Paramter: $new (boolean) ------- #
+# --------- $env ----------------- #
+# -------------------------------- #
+# function run {
 #   #statements
 # }
