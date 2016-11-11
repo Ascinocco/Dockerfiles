f845937265657660494ad10c5e35af7ee184c5a8
Anthony Scinocco <scinocco.a@gmail.com>
2016-11-11 14:08:29 -0500

    Large refactor, using args.list to seed dockerfile based on environment

---
+++ mern/docker/scripts/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#*******************************#
+# ----------build.sh----------- #
+# ---Author: Anthony Scinocco-- #
+# ---Builds the docker image--- #
+# ------November 11, 2016------ #
+#*******************************#
+# Script Parameters: --------------------------------------------------------- #
+# Parameters: ---------------------------------------------------------------- #
+# <new> <prod>: will build the image from scratch with production settings --- #
+# <new> <local>: will build the image from scratch with local settings ------- #
+# <   > <     >: will build the image for local settings --------------------- #
+# ---------------------------------------------------------------------------- #
+# CODE_DIR="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern/code"
+DOCKER_DIR="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
+# ENV_VAL="development" # static for the time being
+ARGS_FILE_PATH="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern/docker/args.list"
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker build --build-arg $ARGS_FILE_PATH -t anthonyscinocco/mern:dev "$DOCKER_DIR"
+
+# -------------------------------- #
+# buildLocal () ------------------ #
+# builds the container for the --- #
+# local environment -------------- #
+# Paramter: $new (boolean) ------- #
+# -------------------------------- #
+# function buildLocal {
+#   #statements
+# }
+
+# function buildProduction {
+#   #statements
+# }
--- mern/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-
-#!/usr/bin/env bash
-
-docker stop $(docker ps -a -q)
-# docker rm $(docker ps -a -q)
-
-docker build -t anthonyscinocco/mern:dev ./local/
