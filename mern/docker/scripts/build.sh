e68ef7c6950df2d4b9db79822eba7b77c998bd44
Anthony Scinocco <scinocco.a@gmail.com>
2016-11-12 12:20:31 -0500

    Cleaning up scripts and dockefile

--- mern/docker/scripts/build.sh
+++ mern/docker/scripts/build.sh
@@ -11,36 +11,27 @@
 # Parameters: $new ----------------------------------------------------------- #
 # ---------------------------------------------------------------------------- #
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+
 APP_PATH="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
 DOCKER_DIR="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
-ARGS_FILE_PATH="./docker/args.dev.list"
 DEV_ARGS='--build-arg NODE_ENV_VAR="development"'
 PROD_ARGS='--build-arg NODE_ENV_VAR="production"'
 
-docker build $(echo $DEV_ARGS) -t anthonyscinocco/mern:latest "$DOCKER_DIR"
-docker run -d -v "$(echo $APP_PATH)":/usr/src \
-        -p 8000:8000 anthonyscinocco/mern:latest
 
-docker logs -f $(docker ps -a -q)
+function stopAndRemoveContainers {
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q)
+}
 
-# -------------------------------- #
-# buildLocal () ------------------ #
-# builds the container for the --- #
-# local environment -------------- #
-# Paramter: $new (boolean) ------- #
-# -------------------------------- #
-# function buildLocal {
-#   #statements
-# }
+function attachLogging {
+  docker logs -f $(docker ps -a -q)
+}
 
-# -------------------------------- #
-# run () ------------------------- #
-# handles control flow ----------- #
-# Paramter: $new (boolean) ------- #
-# --------- $env ----------------- #
-# -------------------------------- #
-# function run {
-#   #statements
-# }
+function run {
+  docker run -d -v "$(echo $APP_PATH)":/usr/src \
+          -p 80:8000 anthonyscinocco/mern:latest
+}
+
+function build {
+  docker build $(echo $DEV_ARGS) -t anthonyscinocco/mern:latest "$DOCKER_DIR"
+}
--- mern/docker/scripts/start.sh
+++ mern/docker/scripts/start.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+# ------------------------------------ #
+# ------------start.sh---------------- #
+# ---Runs the app required processes-- #
+# -------Author: Anthony Scinocco----- #
+# ----------November 12, 2016--------- #
+# ------------------------------------ #
+
+# All processes should run in the background expen npm start
 mongod --dbpath /usr/data/db --fork --logpath /usr/mongo-logs/mongod.log
 npm start
