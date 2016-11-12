0f66ce5cc3c8dd8ee29e10125c64aad2f858f0d1
Anthony Scinocco <scinocco.a@gmail.com>
2016-11-12 02:54:24 -0500

    No luck using supervisor to run both processes
    May have to look into linking a separate container for mongo

--- mern/docker/scripts/run.sh
+++ mern/docker/scripts/run.sh
@@ -6,5 +6,6 @@ APP_PATH="/Users/anthony/Documents/Personal/Projects/2017/docker/docker/mern"
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker run -d -v "$(echo $APP_PATH)":/usr/src \
-        -p 8000:8000 anthonyscinocco/mern:latest \
-        docker logs -f $(docker ps -a -q)
+        -p 8000:8000 anthonyscinocco/mern:latest
+
+docker logs -f $(docker ps -a -q)
