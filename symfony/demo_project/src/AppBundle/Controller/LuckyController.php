d2fb174341dc0a3083e7a0364b2e0d2344dee9f5
Anthony Scinocco <scinocco.a@gmail.com>
2017-01-01 14:19:50 -0500

    playing around with symphony

--- symfony/demo_project/src/AppBundle/Controller/LuckyController.php
+++ symfony/demo_project/src/AppBundle/Controller/LuckyController.php
@@ -4,23 +4,35 @@
    * My first controller
    * Generates a random number
    */
-
    namespace AppBundle\Controller;
 
+   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
    use Symfony\Component\HttpFoundation\Response;
 
-   class LuckyController
+   class LuckyController extends Controller
    {
 
      /**
-      *@Route("/lucky/number")
+      * @Route("/lucky/number")
       */
       public function numberAction()
       {
         $number = mt_rand(0, 100);
 
-        return new Response("<h1>$number</h1>");
+        return new Response("<html><body><h1>$number</h1></body></html>");
       }
+
+      /**
+       * @Route("/lucky/number/twig")
+       */
+       public function twigNumberAction()
+       {
+         $number = mt_rand(0, 100);
+
+         return $this->render('lucky/number.html.twig', array(
+           'number' => $number,
+         ));
+       }
    }
 ?>
